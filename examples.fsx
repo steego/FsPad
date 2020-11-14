e92538fdc3c3428f86786d847b41bd041f9fe5d8
Steve Goguen <sgoguen@gmail.com>
2020-11-14 17:48:19 -0500

    Fixed examples.fsx

--- examples.fsx
+++ examples.fsx
@@ -1,14 +1,15 @@
-#r "nuget: Steego.FsPad"
+#r "nuget:Steego.FsPad,version=0.0.5"
 
 open FsPad
-let browser = FsPad.Web(8080)
-browser.OpenBrowser()
 
-let dump(value) = browser.Dump(value)
+let browser = FsPad.Web(8080)
+//  browser.OpenBrowser()
+
+let dump (value) = browser.Dump(value)
 let dumpLevel level value = browser.Dump(value, level)
 
 #if INTERACTIVE
-let printer(value) = 
+let printer (value) =
     dumpLevel 5 value
     sprintf "%A" value
 
@@ -26,60 +27,102 @@ dump "<b>Hello World</b>"
 dump [ 1 .. 30 ]
 
 // A tuple
-dump ( ("By Plane", 2, 250.99) )
+dump (("By Plane", 2, 250.99))
 
 // A list of tuples (notice how it changes the layout, to tabular)
-dump [ ("By Plane", 2, 250.99); ("By Car", 10, 210.5);  ("By Train", 15, 483.53)  ]
+dump [ ("By Plane", 2, 250.99)
+       ("By Car", 10, 210.5)
+       ("By Train", 15, 483.53) ]
 
 // A record
-type Person = { firstName : string   ; lastName : string; age : int; address : string } 
-dump { firstName = "Gustavo"; lastName = "Leon"; age = 43 ; address = "Dole" }
+type Person =
+    { firstName: string
+      lastName: string
+      age: int
+      address: string }
+
+dump
+    { firstName = "Gustavo"
+      lastName = "Leon"
+      age = 43
+      address = "Dole" }
 
 // A list of records (again changes to tabular)
-dump
-    [
-        {firstName = "Gustavo"; lastName = "Leon"  ; age = 43 ; address = "Dole" }
-        {firstName = "Steve"  ; lastName = "Goguen"; age = 40 ; address = "New Jersey" }
-    ]
+dump [ { firstName = "Gustavo"
+         lastName = "Leon"
+         age = 43
+         address = "Dole" }
+       { firstName = "Steve"
+         lastName = "Goguen"
+         age = 40
+         address = "New Jersey" } ]
 
 // Nested stuff
-type Dev = { firstName : string ; lastName : string ; age : int; address : string ; projects : string list } 
-dump {firstName = "Gustavo"; lastName = "Leon"; age = 43 ; address = "Dole" ; projects = ["F#+"; "ScrapeM" ]}
+type Dev =
+    { firstName: string
+      lastName: string
+      age: int
+      address: string
+      projects: string list }
 
 dump
-    [
-        {firstName = "Gustavo"; lastName = "Leon"  ; age = 43 ; address = "Dole"   ; projects = ["F#+"; "ScrapeM" ; "FsPad" ]}
-        {firstName = "Steve"  ; lastName = "Goguen"; age = 20 ; address = "?" ; projects = ["Steego.NET"; "FsPad" ]}
-    ]
+    { firstName = "Gustavo"
+      lastName = "Leon"
+      age = 43
+      address = "Dole"
+      projects = [ "F#+"; "ScrapeM" ] }
+
+dump [ { firstName = "Gustavo"
+         lastName = "Leon"
+         age = 43
+         address = "Dole"
+         projects = [ "F#+"; "ScrapeM"; "FsPad" ] }
+       { firstName = "Steve"
+         lastName = "Goguen"
+         age = 20
+         address = "?"
+         projects = [ "Steego.NET"; "FsPad" ] } ]
 
 //  Recursively Defined Object
-type Tree<'a>(value:'a, getEdges:'a -> seq<'a>) = 
-    let list = lazy [ for e in getEdges(value) -> Tree(e, getEdges) ]
+type Tree<'a>(value: 'a, getEdges: 'a -> seq<'a>) =
+    let list =
+        lazy ([ for e in getEdges (value) -> Tree(e, getEdges) ])
+
     member this.Value = value
     member this.Children = list.Value
 
-let tree1 = Tree(Some 1, fun (Some x) -> seq { for x in x..2 -> Some x })
+let tree1 =
+    Tree(Some 1, (fun (Some x) -> seq { for x in x .. 2 -> Some x }))
 
 (tree1, 9) |> dumpLevel 12
 
 
 // List of option
-[Some 1; None; Some 4] |> dump
+[ Some 1; None; Some 4 ] |> dump
 
 // List of tuples, some of them are options
-[1..30] |> List.map (fun x -> ( (if x % 2 = 0 then Some x else None), x, if x % 3 = 0 then Some x else None) ) |> dump
+[ 1 .. 30 ]
+|> List.map (fun x -> ((if x % 2 = 0 then Some x else None), x, (if x % 3 = 0 then Some x else None)))
+|> dump
 
 // List of record, some fields are options
-type Tab<'a,'b,'c> = { a : 'a ; b : 'b ; c : 'c }
-[1..30] |> List.map (fun x -> {a = (if x % 2 = 0 then Some x else None); b = x; c = if x % 3 = 0 then Some x else None} ) |> dump
+type Tab<'a, 'b, 'c> = { a: 'a; b: 'b; c: 'c }
+
+[ 1 .. 30 ]
+|> List.map (fun x ->
+    { a = (if x % 2 = 0 then Some x else None)
+      b = x
+      c = if x % 3 = 0 then Some x else None })
+|> dump
 
 //  Loop over
-Async.RunSynchronously <| async {
-    for x in 1..100 do
+Async.RunSynchronously
+<| async {
+    for x in 1 .. 100 do
         do! Async.Sleep(100)
-        dump (x, x * x)
+        dump ({| X = x; Square = x * x |})
 
-    }
+   }
 
 browser.DumpToPath((2, "Hello World.........."), "test/one")
 
